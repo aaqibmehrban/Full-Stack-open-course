ece64436b6b2f99b7df8811bf19d48e1343a408a
Aaqib Mehrban <46885712+aaqibmehrban@users.noreply.github.com>
2023-12-16 01:19:12 +0200

    2.8: The Phonebook Step3

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -3,16 +3,18 @@ import { useState } from 'react'
 const Phonebook = (props) => {
   return (
     <div>
-      <p>{props.name}</p>
+      <p>{props.name} {props.phone}</p>
     </div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+  number: '050-1234567' },
   ]) 
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const addPhone = (event) => {  
     event.preventDefault()
@@ -21,9 +23,11 @@ const App = () => {
     if (!persons.some(element => element.name === inputVal)){
         const noteObject = {
           name: newName,
+          number: newPhone,
       }
       setPersons(persons.concat(noteObject))
       setNewName('')
+      setNewPhone('')
     }
     else{
       alert(`${inputVal} is already added to phonebook`)
@@ -35,22 +39,24 @@ const App = () => {
     console.log(event.target.value)    
     setNewName(event.target.value) 
    }
+  const handlePhoneChange = (event) => {    
+  console.log(event.target.value)    
+  setNewPhone(event.target.value) 
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPhone}>
-        <div>
-          name: <input id='input-field' value={newName} onChange={handleInputChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input id='input-field' value={newName} onChange={handleInputChange}/></div>
+        <div>number: <input id='input-phone' value={newPhone} onChange={handlePhoneChange}/></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <div>
       {persons.map(p => 
-          <Phonebook key={p.name} name={p.name} />
+          <Phonebook key={p.name} name={p.name} phone={p.number}/>
         )}
       </div>
     </div>
