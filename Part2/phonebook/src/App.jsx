6689b7a396581b76c6c7cedb8a262f8673c4d083
Aaqib Mehrban <46885712+aaqibmehrban@users.noreply.github.com>
2023-12-16 02:45:47 +0200

    2.9*: The Phonebook Step4

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -10,11 +10,14 @@ const Phonebook = (props) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-  number: '050-1234567' },
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filterPhone,setFilterPhone] = useState(persons)
 
   const addPhone = (event) => {  
     event.preventDefault()
@@ -45,9 +48,17 @@ const App = () => {
   }
 
 
+  const filterPhoneBook = (event) =>{
+    setFilterPhone(persons.filter((element)=>element.name.toLowerCase().includes(event.target.value.toLowerCase()))  )
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={filterPhoneBook}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPhone}>
         <div>name: <input id='input-field' value={newName} onChange={handleInputChange}/></div>
         <div>number: <input id='input-phone' value={newPhone} onChange={handlePhoneChange}/></div>
@@ -55,7 +66,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-      {persons.map(p => 
+      {filterPhone.map(p => 
           <Phonebook key={p.name} name={p.name} phone={p.number}/>
         )}
       </div>
